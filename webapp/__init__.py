cd71d0a5d8e12add1d934ab4de77eca60f04db22
Artur Gazizov <a.gazizov883@yandex.ru>
2022-06-29 22:47:13 +0500

    All project

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -1,14 +1,12 @@
-import app
 from flask import Flask, render_template
-from webapp.model import Route, Coordinate
+from webapp.model import Route, Coordinate, Detail, Visual
 from webapp.extensions import db, migrate
-from webapp.admin import RouteImageView, form, CoordinateModelView
+from webapp.admin import RouteImageView, form, CoordinateModelView, DetailModelView, VisualModelView
 from flask_admin import Admin
 from webapp.weather import weather_by_city
 import folium
 from folium.plugins import MarkerCluster
 from folium import IFrame
-import os
 import base64
 
 
@@ -22,37 +20,34 @@ def create_app():
     admin = Admin(app, name='map_rout', template_mode='bootstrap4')
     register_admin_views(admin)
 
-
     @app.route('/')
-    def index():        
-
+    def index():
         map_rout = Route.query.all()
         weather = weather_by_city("Sochi, Russia")
         return render_template("index.html", map_rout=map_rout, thumbnail=form.thumbgen_filename, weather=weather)
 
     @app.route('/<int:pk>')
     def detail(pk):
-        
         coordinates = Coordinate.query.filter_by(route_id=pk).all()
-        start_location=next(iter(coordinates))
+        start_location = next(iter(coordinates))
         if not start_location:
-            raise NotFound
+            raise 'NotFound'
         
         folium_map=folium.Map(location=[start_location.latitude, start_location.longitude],
-        zoom_start=10,
-        width=1000,
-        height=600,
-        left=200,
-        top=80) 
+                            zoom_start=10,
+                            width=1000,
+                            height=600,
+                            left=200,
+                            top=80)
 
         title = '<h2>Начало маршрута<h2/>'
         picture = base64.b64encode(open('./webapp/static/palatka/ko.png', 'rb').read()).decode()
         html = f'{title}<img src="data:image/JPG;base64,{picture}">'
-        iframe = IFrame(html, width=632 + 20, height=420 + 20)
+        iframe = IFrame(html, width=632+20, height=420+20)
         popup = folium.Popup(iframe, max_width=1000)
-        
+
         folium.Marker(
-        location=[43.6949, 40.3554 ],
+        location=[43.6949, 40.3554],
         popup=popup,
         icon=folium.Icon(icon='glyphicon-home', color="red"),
         draggable=False).add_to(folium_map)
@@ -63,7 +58,7 @@ def create_app():
         draggable=False).add_to(folium_map)
 
         maps_routes = Route.query.filter_by(id=pk).first()
-        return render_template("detail.html", maps_routes=maps_routes,folium_map=folium_map._repr_html_())
+        return render_template("detail.html", maps_routes=maps_routes ,folium_map=folium_map._repr_html_())
 
     return app
 
@@ -71,10 +66,11 @@ def create_app():
 def register_admin_views(admin):
     admin.add_view(RouteImageView(Route, db.session))
     admin.add_view(CoordinateModelView(Coordinate, db.session))
+    admin.add_view(DetailModelView(Detail, db.session))
+    admin.add_view(VisualModelView(Visual, db.session))
 
 
 def register_extensions(app):
     db.init_app(app)
     migrate.init_app(app, db)
     return None
-
--- webapp/model.py
+++ webapp/model.py
@@ -7,9 +7,12 @@ class Route(db.Model):
     description = db.Column(db.Text(), nullable=False)
     image = db.Column(db.Unicode(128), nullable=False)
     coordinates = db.relationship('Coordinate', backref='route', lazy=True)
+    detail = db.relationship('Detail', backref='route', lazy=True)
+    visuals = db.relationship('Visual', backref='route', lazy=True)
 
     def __repr__(self):
         return f'<Route {self.title}>'
+
 
 class Coordinate(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -20,4 +23,28 @@ class Coordinate(db.Model):
     order = db.Column(db.Integer, nullable=False)
 
     def __repr__(self):
-        return f'<Coordinate {self.route_id}>'
+        return f'<Coordinate {self.longitude}>'
+
+
+class Detail(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    route_id = db.Column(db.Integer, db.ForeignKey('route.id'), nullable=False)
+    description = db.Column(db.Text(), nullable=False)
+    image = db.Column(db.Unicode(128), nullable=False)
+    routestart = db.Column(db.Text(), nullable=False)
+    workingmode = db.Column(db.Text(), nullable=False)
+    order = db.Column(db.Integer, nullable=False)
+
+    def __repr__(self):
+        return f'<Detail {self.description}>'
+
+
+class Visual(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    img = db.Column(db.Unicode(128), nullable=False)
+    title = db.Column(db.String(80), nullable=False)
+    body = db.Column(db.Text(), nullable=False)
+    route_id = db.Column(db.Integer, db.ForeignKey('route.id'), nullable=False)
+
+    def __repr__(self):
+        return f'<Visual {self.title}>'
