c0c4f78833efc71983413e3d364d424bc0329383
Rahmon22SoH <mebelnazakazms@gmail.com>
2022-08-09 22:23:33 +0300

    add likes

--- webapp/config.py
+++ webapp/config.py
@@ -2,4 +2,10 @@ import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, '..', 'db.sqlite3')
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir,
+                                                      '..', 'db.sqlite3')
+SQLALCHEMY_TRACK_MODIFICATIONS = False
+WEATHER_DEFAULT_CITY = "Sochi,Russia"
+WEATHER_API_KEY = "55d82c1a6ba2440eb58133738220506"
+WEATHER_URL = "http://api.worldweatheronline.com/premium/v1/weather.ashx"
+SECRET_KEY = "AWHDPDAIJWDIOJDsmakmksmcikejdfwejfunas_^##^@^5^35862135"
