57e63ff83c2481c88c8c37d6b7d9b73a207f2221
Artur Gazizov <a.gazizov883@yandex.ru>
2022-09-06 19:20:33 +0500

    Add ignore file

--- webapp/static/js/admin.js
+++
@@ -1,8 +0,0 @@
-ClassicEditor
-    .create( document.querySelector( '.ckeditor' ) )
-    .then( editor => {
-        console.log( editor );
-    } )
-    .catch( error => {
-        console.error( error );
-    } );
--- webapp/static/js/detail.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll('oembed[url]').forEach(element => {
-    const anchor = document.createElement('a');
-
-    anchor.setAttribute('href', element.getAttribute('url'));
-    anchor.className = 'embedly-card';
-
-    element.appendChild(anchor);
-});
